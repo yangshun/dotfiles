35e587db52cf96263e950aecd7557efc874ef11e
Yangshun Tay <yangshun@fb.com>
2017-11-21 21:18:11 -0800

    Update for work MBP

--- new-setup.sh
+++ new-setup.sh
@@ -1,8 +1,23 @@
+#################
+# Installations #
+#################
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
+# Install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#################
+# Configuration #
+#################
+
 # Show hidden files in Mac
 defaults write com.apple.Finder AppleShowAllFiles true
-
-# Sublime text binary alias
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+killall Finder /System/Library/CoreServices/Finder.app
 
 # Change default screenshots location
 mkdir ~/Screenshots & defaults write com.apple.screencapture location ~/Screenshots
